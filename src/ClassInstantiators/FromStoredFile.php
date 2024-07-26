c04049be6caeb1e5adf66b5a7aa62f447e85a1bf
pjordaan <pjordaan@users.noreply.github.com>
2024-07-26 06:24:58 +0000

    Update file(s) "packages/storage-metadata/." from "apie-lib/apie-lib-monorepo"

--- src/ClassInstantiators/FromStoredFile.php
+++ src/ClassInstantiators/FromStoredFile.php
@@ -1,6 +1,7 @@
 <?php
 namespace Apie\StorageMetadata\ClassInstantiators;
 
+use Apie\Core\FileStorage\StoredFile;
 use Apie\StorageMetadata\Interfaces\ClassInstantiatorInterface;
 use Apie\StorageMetadata\Interfaces\StorageDtoInterface;
 use Psr\Http\Message\UploadedFileInterface;
@@ -10,6 +11,9 @@ final class FromStoredFile implements ClassInstantiatorInterface
 {
     public function supports(ReflectionClass $class, ?StorageDtoInterface $storageObject = null): bool
     {
+        if ($class->name === UploadedFileInterface::class) {
+            return true;
+        }
         if ($storageObject === null) {
             return false;
         }
@@ -19,7 +23,11 @@ final class FromStoredFile implements ClassInstantiatorInterface
     public function create(ReflectionClass $class, ?StorageDtoInterface $storageObject = null): object
     {
         assert($storageObject !== null);
-        return $storageObject->getClassReference()->getMethod('createFromDto')
+        $classReference = $storageObject->getClassReference();
+        if ($classReference->name === UploadedFileInterface::class) {
+            $classReference = new ReflectionClass(StoredFile::class);
+        }
+        return $classReference->getMethod('createFromDto')
             ->invoke(null, $storageObject);
     }
 }
